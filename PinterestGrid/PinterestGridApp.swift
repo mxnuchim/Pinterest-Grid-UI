6ddf7c042417a2bf1398c60b55d6fc02c823976f
mxnuchim <manuchimoliver779@gmail.com>
2023-02-22 13:32:00 +0100

    Initial Commit

---
+++ PinterestGrid/PinterestGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestGridApp.swift
+//  PinterestGrid
+//
+//  Created by Manuchim Oliver on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinterestGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
